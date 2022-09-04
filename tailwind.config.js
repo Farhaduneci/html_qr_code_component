d9a3fe6f04c2226eb67e469f7f1a0d88a07a22c8
Farhad <farhaduneci@gmail.com>
2022-09-04 16:35:24 +0430

    UPDATE: Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+        sans: ["Outfit", ...defaultTheme.fontFamily.sans],
       },
     },
   },
